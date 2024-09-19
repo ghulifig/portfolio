ee631e8a60f8b79f7d905501e97de97e4b5e932a
Ghuliana Figueredo <ghulianafig182@gmail.com>
2024-09-19 12:00:39 -0400

    fix tailwind

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,26 +4,27 @@ export default function Home() {
   return (
     <section class="home" id="home">
         <div class="home-content">
-            <div style={{ display: 'flex', width: '100%' }}>
-              <div style={{ width: '50%' }}>
-                <h3>Ghuliana Figueredo</h3>
-                <h4>Web Developer</h4>
-                <br/>
-                <div className="about_me" id="about_me">
-                  I’m a full stack developer with over two years of experience in developing web applications.<br/><br/>
-                  My approach focuses on creating digital experiences that are fast, accessible, visually appealing, and responsive, while integrating tools and practices that optimize the efficiency and scalability of the projects I work on.
-                </div>
-              </div>
-              <div style={{ width: '50%' }}>
-                <div className="luffy" id="luffy">
-                  <img src={batman('./peeker_luffy2.png')} alt="Python" style={{ maxWidth: '40%' }} />
-                </div>
-                <div>
-                  <a href="https://www.mangago.me/read-manga/one_piece/mr/one-piece/1/" target="_blank" className="btn-resume">Download resume</a>
-                </div>
+          <div style={{ display: 'flex', width: '100%' }}>
+            <div style={{ width: '50%' }}>
+              <h3>Ghuliana Figueredo</h3>
+              <h4>Web Developer</h4>
+              <br/>
+              <div className="about_me" id="about_me">
+                I’m a full stack developer with over two years of experience in developing web applications.<br/><br/>
+                My approach focuses on creating digital experiences that are fast, accessible, visually appealing, and responsive, while integrating tools and practices that optimize the efficiency and scalability of the projects I work on.
               </div>
             </div>
-           
+            <div style={{ width: '50%' }}>
+              <div className="luffy" id="luffy">
+                <img src={batman('./peeker_luffy2.png')} alt="Python" style={{ maxWidth: '40%' }} />
+              </div>
+              <div>
+                <a href="https://www.mangago.me/read-manga/one_piece/mr/one-piece/1/" target="_blank" className="btn-resume">Download resume</a>
+              </div>
+            </div>
+            {/* <button class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">ola
+              </button> */}
+          </div>
         </div>
         {/* <div class="home-img">
             <img src="https://toppng.com/uploads/preview/luffy-2nd-gear-one-piece-luffy-gear-second-11562941039pcqlhzgq9t.png" alt="hola" width="400px" height="400px"></img>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,25 @@ export default function Navbar() {
     <header class="header">
         <a href="#" class="logo">&lt;GF/&gt;</a>   
         {/* <i class="fa fa-bars" aria-hidden="true" id="menu-icon"></i>*/}
-        <nav class="navbar">
-            <a href="#home" class="active">Home</a>
-            {/* <a href="#about">About</a> */}
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
-        </nav>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-sun">
-                    <circle cx="12" cy="12" r="4"></circle>
-                    <path d="M12 2v2"></path>
-                    <path d="M12 20v2"></path>
-                    <path d="m4.93 4.93 1.41 1.41"></path>
-                    <path d="m17.66 17.66 1.41 1.41"></path>
-                    <path d="M2 12h2"></path><path d="M20 12h2"></path>
-                    <path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path>
-                </svg>
+        <div>
+            <nav class="navbar">
+                <a href="#home" class="active">Home</a>
+                {/* <a href="#about">About</a> */}
+                <a href="#skills">Skills</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </nav>
+            <hr className="border-l my-6 borde_popo" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-sun">
+                <circle cx="12" cy="12" r="4"></circle>
+                <path d="M12 2v2"></path>
+                <path d="M12 20v2"></path>
+                <path d="m4.93 4.93 1.41 1.41"></path>
+                <path d="m17.66 17.66 1.41 1.41"></path>
+                <path d="M2 12h2"></path><path d="M20 12h2"></path>
+                <path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path>
+            </svg>
+        </div>
     </header>
     );
 }
